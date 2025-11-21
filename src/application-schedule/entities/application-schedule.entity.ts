a7f9b607d0490372c335d0ff01ecce10fe060c90
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 01:20:43 -0300

    fix: remove import of unused Document and Types in application-schedule.entity.ts

--- src/application-schedule/entities/application-schedule.entity.ts
+++ src/application-schedule/entities/application-schedule.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, HydratedDocument, Types } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export enum ApplicationStatus {
   SCHEDULED = 'SCHEDULED',
